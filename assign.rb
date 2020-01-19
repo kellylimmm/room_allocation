f17a454624a8778ca10f8e9fd4fb752f1476c572
“kellylimmm” <kellylim.06@gmail.com>
2020-01-20 03:43:16 +0800

    Completed

--- assign.rb
+++ assign.rb
@@ -50,23 +50,23 @@ num_of_rooms = 3
 
 def get_dates(bookings)
 
-    earliest_checkin = Date.parse(bookings.first[:checkin])
-    latest_checkin = Date.parse(bookings.first[:checkin])
+  earliest_checkin = Date.parse(bookings.first[:checkin])
+  latest_checkin = Date.parse(bookings.first[:checkin])
 
-    bookings.each do |booking|
-      checkin = Date.parse(booking[:checkin])
-      # find the earliest checkin date
-      if checkin < earliest_checkin
-        earliest_checkin = checkin
-      end
-
-      # find the latest checkin date
-      if checkin > latest_checkin
-        latest_checkin = checkin
-      end
+  bookings.each do |booking|
+    checkin = Date.parse(booking[:checkin])
+    # find the earliest checkin date
+    if checkin < earliest_checkin
+      earliest_checkin = checkin
     end
 
-return earliest_checkin, latest_checkin
+    # find the latest checkin date
+    if checkin > latest_checkin
+      latest_checkin = checkin
+    end
+  end
+
+  return earliest_checkin, latest_checkin
 
 end
 
@@ -80,9 +80,7 @@ def assign_rooms(bookings, num_of_rooms)
   rooms.each do |room|
 
     earliest_checkin, latest_checkin = get_dates(bookings)
-
     date = earliest_checkin
-
     while date <= latest_checkin
 
       bookings.each do |booking|
@@ -104,7 +102,6 @@ def assign_rooms(bookings, num_of_rooms)
   end
 
   print rooms
-
 end
 
 result = assign_rooms(bookings, num_of_rooms)
